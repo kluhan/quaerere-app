3f8b40b5ec8f0144b1c296fd659b043e37bcccfa
Klaus Luhan <klaus.luhan@gmail.com>
2019-11-28 16:53:54 +0100

    Refactored GoogleComponent
    Refactored GoogleComponent

--- src/app/demographic/google/google.component.ts
+++ src/app/demographic/google/google.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Store, } from '@ngxs/store';
-import { SetAge, SetGender, SetCountry, SetEducation, SetIncome, SetName, SetProfession } from 'src/app/actions/demographicData.action';
-import { AgeScale, AgeRange20 } from 'src/app/models/demographic/age.enum';
-import { GenderScale, GenderBasic, GenderAdvanced } from 'src/app/models/demographic/gender.enum';
-import { CountryScale, DeuAutChe } from 'src/app/models/demographic/country.enum';
-import { EducationGerman, EducationScale, EducationAcademic } from 'src/app/models/demographic/education.enum';
+import { AgeScale } from 'src/app/models/demographic/age.enum';
+import { GenderScale } from 'src/app/models/demographic/gender.enum';
+import { CountryScale } from 'src/app/models/demographic/country.enum';
+import { EducationScale } from 'src/app/models/demographic/education.enum';
 import { IncomeScale } from 'src/app/models/demographic/income.enum';
-import { ProfessionBasic, ProfessionScale, ProfessionAdvanced } from 'src/app/models/demographic/profession.enum';
 
 @Component({
   selector: 'app-google',
@@ -18,14 +16,6 @@ export class GoogleComponent implements OnInit {
 
   googleForm: FormGroup;
 
-  genderKeys: String[];
-  genderLabelGenerator = GenderAdvanced;
-
-  ageKeys: String[];
-  ageLabelGenerator = AgeRange20;
-
-  countryKeys: String[];
-
   ageScale = AgeScale.RANGE_20;
   genderScale = GenderScale.BASIC;
   countryScale = CountryScale.DEU_AUT_CHE;
@@ -35,36 +25,11 @@ export class GoogleComponent implements OnInit {
   constructor(private store: Store, private fb: FormBuilder) {}
 
   async ngOnInit() {
-    this.genderKeys = Object.keys(GenderAdvanced);
-    this.ageKeys = Object.keys(AgeRange20);
-    this.countryKeys = Object.keys(DeuAutChe);
 
     this.googleForm = this.fb.group({
       company: 'Google',
-      state: '',
-      firstName: '',
-      lastName: '',
     });
 
-
-    const firstName = this.googleForm.get('firstName');
-    const lastName = this.googleForm.get('lastName');
-
-    firstName.valueChanges.subscribe(() => this.store.dispatch(new SetName(firstName.value, lastName.value)));
-    lastName.valueChanges.subscribe(() => this.store.dispatch(new SetName(firstName.value, lastName.value)));
-
-    this.store.dispatch(new SetAge(AgeRange20.BELOW_20, AgeScale.RANGE_20));
-    this.store.dispatch(new SetGender(GenderBasic.MALE, GenderScale.BASIC));
-    this.store.dispatch(new SetAge(25, AgeScale.BASIC));
-    this.store.dispatch(new SetCountry(DeuAutChe.AUSTRIA, CountryScale.DEU_AUT_CHE));
-    this.store.dispatch(new SetGender(GenderAdvanced.DIVERSE, GenderScale.ADVANCED));
-    this.store.dispatch(new SetEducation(EducationGerman.HOCHSCHULREIFE, EducationScale.GERMAN));
-    this.store.dispatch(new SetCountry(DeuAutChe.AUSTRIA, CountryScale.DEU_AUT_CHE));
-    this.store.dispatch(new SetEducation(EducationAcademic.BACHELOR, EducationScale.ACADEMIC));
-    this.store.dispatch(new SetIncome(50000, IncomeScale.RANGE_1000));
-    this.store.dispatch(new SetIncome(1200, IncomeScale.BASIC));
-    this.store.dispatch(new SetProfession(ProfessionBasic.STUDENT, ProfessionScale.BASIC));
-    this.store.dispatch(new SetProfession(ProfessionAdvanced.FIREFIGHTER, ProfessionScale.ADVANCED));
   }
 
 }
