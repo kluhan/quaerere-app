bde5f5999a20597f90e6302f45627aa24a303952
Klaus Luhan <klaus.luhan@gmail.com>
2019-12-05 20:09:29 +0100

    Added LikertScale
    Added a basic five and three level LikertScale

---
+++ src/app/share/enumerations/likert.enum.ts
@@ -0,0 +1,18 @@
+export enum LikertFiveLevel {
+    STRONG_AGREE = 'strong_disagree',
+    AGREE = 'agree',
+    NEUTRAL = 'neutral',
+    DISAGREE = 'disagree',
+    STRONG_DISAGREE = 'strong_disagree',
+}
+
+export enum LikertThreeLevel {
+    AGREE = 'agree',
+    NEUTRAL = 'neutral',
+    DISAGREE = 'disagree',
+}
+
+export enum LikertScale {
+    LIKERT_FIVE_LEVEL,
+    LIKERT_THREE_LEVEL
+}
