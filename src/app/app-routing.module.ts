4eb5d4b5e8434a2e4e0fdea8b5638170dbdf4b06
Klaus Luhan <klaus.luhan@gmail.com>
2020-01-05 18:43:53 +0100

    Removed DummyTests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GoogleComponent } from './demographic/google/google.component';
 import { FacebookComponent } from './demographic/facebook/facebook.component';
-import { FoodFrequencyComponent } from './tests/food-frequency/food-frequency.component';
-import { CarCostComponent } from './tests/car-cost/car-cost.component';
 import { LinkerComponent } from './linker/linker.component';
 import { NeoFfiComponent } from './tests/neo-ffi/neo-ffi.component';
 import { SelectorComponent } from './selector/selector.component';
@@ -12,8 +10,6 @@ const routes: Routes = [
   { path: 'demographic/google', component: GoogleComponent },
   { path: 'demographic/facebook', component: FacebookComponent },
   { path: 'linker', component: LinkerComponent },
-  { path: 'tests/food-frequency', component: FoodFrequencyComponent},
-  { path: 'tests/car-cost', component: CarCostComponent},
   { path: 'tests/neo-ffi', component: NeoFfiComponent},
   { path: 'selector', component: SelectorComponent},
 ];
--- src/app/linker/linker.component.ts
+++ src/app/linker/linker.component.ts
@@ -1,7 +1,5 @@
 import { Component, ViewChild, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { GoogleComponent } from '../demographic/google/google.component';
-import { CarCostComponent } from '../tests/car-cost/car-cost.component';
-import { FoodFrequencyComponent } from '../tests/food-frequency/food-frequency.component';
 import { NeoFfiComponent } from '../tests/neo-ffi/neo-ffi.component';
 import { Tests } from '../share/enumerations/tests.enum';
 import { Demographic } from '../share/enumerations/demographic.enum';
@@ -27,8 +25,6 @@ export class LinkerComponent implements AfterViewInit {
 
    @ViewChild(GoogleComponent, {static: false}) googleComponent: GoogleComponent;
    @ViewChild(FacebookComponent, {static: false}) facebookComponent: FacebookComponent;
-   @ViewChild(CarCostComponent, {static: false}) carCostComponent: CarCostComponent;
-   @ViewChild(FoodFrequencyComponent, {static: false}) foodFrequencyComponent: FoodFrequencyComponent;
    @ViewChild(NeoFfiComponent, {static: false}) neoFfiComponent: NeoFfiComponent;
    @ViewChild(MpZmComponent, {static: false}) mpZmComponent: MpZmComponent;
 
@@ -38,14 +34,6 @@ export class LinkerComponent implements AfterViewInit {
 
    get frmFacebookComponent() {
       return this.facebookComponent ? this.facebookComponent.formGroup : null;
-   }
-
-   get frmCarCostComponent() {
-      return this.carCostComponent ? this.carCostComponent.formGroup : null;
-   }
-
-   get frmFoodFrequencyComponent() {
-      return this.foodFrequencyComponent ? this.foodFrequencyComponent.formGroup : null;
    }
 
    get frmNeoFfiComponent() {
