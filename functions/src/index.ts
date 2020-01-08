02de7f072642d64933aabcc82816f6a8f64763a5
Klaus Luhan <klaus.luhan@gmail.com>
2020-01-08 14:50:07 +0100

    CustomTokenCloudFunction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+const  uidgenerator = require('uid-generator');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require ('firebase-admin');
+
+const uidgen = new uidgenerator(); // Default is a 128-bit UID encoded in base58
+ 
+admin.initializeApp({
+    serviceAccountId: 'quaerere-app@appspot.gserviceaccount.com',
+    ...functions.config().firebase,
+});
+
+exports.getToken = functions.https.onRequest((request, response) => {
+    const token = request.query.token;
+    const tokenDoc = admin.firestore().collection('token').doc(token);
+    tokenDoc.get().then((doc) => {
+        if (doc.exists) {
+            uidgen.generate()
+            .then((uid: string) => {
+                admin.auth().createCustomToken(uid)
+                    .then(
+                        response.send({uid, doc})
+                    )
+            })
+        } else {
+            response.send(404);
+        }
+        
+    })
+});
+
+    
+
